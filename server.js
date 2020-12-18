73f0ca1f1db242430db5c077778853ad6f8ddc64
Srijan Nayak <srijannayak0@gmail.com>
2020-12-18 20:38:15 +0530

    feat: Initial commit setting up express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const port = 8080;
+const server = app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}/`);
+});
