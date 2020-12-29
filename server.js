fb9db042a2f8d28dd3b3e2442ec686d6c56b1213
Srijan Nayak <srijannayak0@gmail.com>
2020-12-29 19:30:54 +0530

    feat: Set up GET route to get projectData

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.use(bodyParser.json());
 const cors = require("cors");
 app.use(cors());
 
+const projectData = [];
+
+// router for handling handle requests to /entries/*
+const entriesRouter = express.Router();
+app.use("/entries", entriesRouter);
+
+// GET route for entries; returns the global project data
+entriesRouter.get("/", (req, res) => {
+  res.send(projectData);
+  console.log("Data fetched through GET route:");
+  console.log(projectData);
+});
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}/`);
